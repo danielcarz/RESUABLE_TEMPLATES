6ef078067e9b9914615272ce7503d9c37f074185
danielcarz <dcz9412@gmail.com>
2023-09-25 13:11:12 -0500

    commit

--- src/COMPONENTS/Navbar.jsx
+++ src/COMPONENTS/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink} from "react-router-dom";
+
+//styles
+import '../SASS/COMPONENTS/REUSABLE_COMPONENTS/Navbar.scss';
 
 export const Navbar = () => {
+
+    
+
+   /*  const activate = ({isActive}) => {
+        console.log('the activate function executed');
+        if(isActive === true){
+            return 'activate'
+        }
+    } */
 
 
     return(
         <nav>
             <ul>
-                <NavLink to={"/"}>PageOne</NavLink>
+                <NavLink to={"/"}  activeclassName="activate">PageOne</NavLink>
+                <NavLink to={"/two"} className={() => {console.log('hello class')}}>PageTWO</NavLink>
             </ul>
         </nav>
     )
