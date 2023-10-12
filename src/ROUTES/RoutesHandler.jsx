a5a4d037f003bbe02b0be81e5c1feafa480c23cc
danielcarz <dcz9412@gmail.com>
2023-10-12 12:30:05 -0500

    acceptiong incoming changes in RoutesHandler

--- src/ROUTES/RoutesHandler.jsx
+++ src/ROUTES/RoutesHandler.jsx
@@ -4,12 +4,8 @@ import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
 import { UserProvider ,Navbar} from './'
 
 //Routes
-<<<<<<< HEAD
-import {PageOne, PageTWO, PageThree } from './';
-=======
 import {TemplateHome, ColombianCulture } from './';
 
->>>>>>> TESTING
 
 export const RoutesHandler = () => {
 
@@ -22,21 +18,12 @@ export const RoutesHandler = () => {
                 <Navbar></Navbar>
 
                 <Routes>
-<<<<<<< HEAD
-                    <Route path="/" element={<PageOne></PageOne>}></Route>
-                    <Route path="/two" element={<PageTWO></PageTWO>}></Route>
-                    <Route path="/three" element={<PageThree></PageThree>}></Route>
-
-
-                    <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
-=======
                     <Route path="/" element={<TemplateHome></TemplateHome>}></Route>
                     <Route path="/two" element={<ColombianCulture></ColombianCulture>}></Route>
 
                     
 
                     
->>>>>>> TESTING
                 </Routes>
 
 
